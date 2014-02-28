bc6d64080f6804960b3f3a008fa82bbfa5e4d363
spencer handley <spencerhandley@gmail.com>
2014-02-27 20:20:30 -0800

    new schtuff

--- app.js
+++ app.js
@@ -4,25 +4,31 @@ fs = require('fs');
 my_http.createServer(function(request,response){   
    console.log("Recieved request:" + request.url);
    var route = router[request.url];
+   var my_404 = router["/404"]
    if(route) {
-    route(request,response);}
+    route(request,response);
+  }
   else {
-    response.writeHead(404,{"Content-type":"text/plain"});
-    response.end("Sorry the page was not found");
+    my_404(request, response);
     }
 }).listen(8080);   
 sys.puts("Server Running on 8080");  
 
 var router = {
   "/": function (request, response) {
-  fs.readFile("views/index.html",function(error,data){
-    if(error){
-        response.writeHead(404,{"Content-type":"text/plain"});
-        response.end("Sorry the page was not found");
-    }else{
-        response.writeHead(202,{"Content-type":"text/html"});
-        response.end(data);
-    }
-  });
-}
+    fs.readFile("views/index.html",function(error,data){
+      if(error){
+          route("/404", response);
+      }else{
+          response.writeHead(202,{"Content-type":"text/html"});
+          response.end(data);
+      }
+    });
+  },
+  "/404": function(request, response) {
+    fs.readFile("views/404.html", function (error, data) {
+      response.writeHead(202,{"Content-type":"text/html"});
+      response.end(data);
+    });
+  }
 };
